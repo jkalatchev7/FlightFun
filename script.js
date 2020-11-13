e593d4ba37672aa014882e695e76beca1cf4acf6
Marko Stepniczka <markostepniczka@Markos-MacBook-Pro.local>
2020-11-13 14:42:02 -0500

    make branch

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 
 //var searchButton = document.getElementById('destination');
 let text = document.getElementById('origin');
+let date = document.getElementById('date');
 
 function submitForm() {
     let tex = text.value;
+    let dat = date.value;
     console.log(tex);
     document.getElementById("firstBox").style.display = "block";
-    appendPre(tex);
+    appendPre(tex, dat);
 }
 
-function appendPre(message) {
+function appendPre(message, date) {
     let d = document.createElement('div');
     d.setAttribute("class", "entry");
-    let t = document.createTextNode("We found flights to: " + message + "\n");
+    let t = document.createTextNode("We found flights to: " + message + " on " + date + "\n");
     //document.getElementById("firstBox").setAttribute("textContent", ("Showing flights departing: " + message));
+    //blah
     d.appendChild(t);
     document.body.appendChild(d);
 
