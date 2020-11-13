8dfafcc67896c4f361d37d9d95421108107c7102
Marko Stepniczka <markostepniczka@Markos-MacBook-Pro.local>
2020-11-13 18:57:30 -0500

    push

--- script.js
+++ script.js
@@ -6,19 +6,24 @@ let date = document.getElementById('date');
 
 function getData(origin, code, date) {
     const url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/" + origin + "-sky/" + code + "-sky/" + date;
-fetch(url, {
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": url,
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "8334b1be82msh6b0193ec25e329ap1d5320jsnbe501d429b64",
 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
 	}
-})
-.then(response => {
-	console.log(response.json());
-})
-.catch(err => {
-	console.error(err);
+};
+let arr;
+$.ajax(settings).done(function (response) {
+	//return response;
+    //return [response["Quotes"][0]]
+    arr = [response["Quotes"][0]["MinPrice"], response["Quotes"][0]["Direct"], response["Carriers"][0]["Name"]];
+	//console.log()
 });
+return arr;
 }
 
 function submitForm() {
@@ -30,7 +35,9 @@ function submitForm() {
     let listCodes = ["SFO", "FLL"];
     let j = listCodes.length - 1;
     while (j >= 0) {
-        getData(tex, listCodes[j], dat);
+        let data = getData(tex, listCodes[j], dat);
+        //let data = getData(tex, listCodes[j], dat);
+        console.log(data);
         appendPre(listCodes[j], dat);
         j--;
     }
