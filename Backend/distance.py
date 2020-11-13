c85b364589f9e6b8a7fabe2167a73623b1e66a10
Jkalatchev7 <Jkalatchev3@gatech.edu>
2020-11-13 12:49:58 -0500

    removing empty files

--- Backend/distance.py
+++
@@ -1,5 +0,0 @@
-#Need to find decent API for finding distance
-#Google Distance Matrix doesn't work bc it uses roads and other networks not flight distance
-#distanceAPI only allows 100 requests per month
-
-# Example Change
