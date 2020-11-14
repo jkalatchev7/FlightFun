514877dc91a6810425d54a467123be9544031fa4
Marko Stepniczka <markostepniczka@Markos-MacBook-Pro.local>
2020-11-13 20:34:39 -0500

    adding to def

--- script.js
+++ script.js
@@ -17,8 +17,15 @@ xhr.setRequestHeader("x-rapidapi-key", "8334b1be82msh6b0193ec25e329ap1d5320jsnbe
 xhr.setRequestHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com");
 
 xhr.send(data);
+var check = setTimeout( function() {alert("Timeout")}, 2000);
+while (xhr.readyState !== 4) {
+}
+clearTimeout(check);
+
 let response = JSON.parse(xhr.responseText);
+console.log(response);
 arr = [response["Quotes"][0]["MinPrice"], response["Quotes"][0]["Direct"], response["Carriers"][0]["Name"]];
+
 return arr;
 }
 
@@ -28,21 +35,30 @@ function submitForm() {
     console.log(tex);
     document.getElementById("firstBox").style.display = "block";
     document.getElementById("firstBox").innerText = ("Showing flights departing " + tex + " on " + dat + "\n");
-    let listCodes = ["SFO", "FLL"];
+    let listCodes = ["BOS", "IAD", "SFO", "ATL"];
     let j = listCodes.length - 1;
     while (j >= 0) {
-        let data = getData(tex, listCodes[j], dat);
-        console.log(data);
-        appendPre(listCodes[j], dat);
+        //console.log("comparing: " + listCodes[j] + " and " + tex);
+        if (listCodes[j] !== tex) {
+            let data = getData(tex, listCodes[j], dat);
+            console.log(data);
+            appendPre(listCodes[j], dat, data[0], data[1], data[2]);
+        }
         j--;
     }
 }
 
-function appendPre(message, date) {
+function appendPre(message, date, minprice, direct, carrier) {
     let d = document.createElement('div');
     d.setAttribute("class", "entry");
     let num = .056;
-    let t = document.createTextNode(message + " $100" + " Only " + num + " dollars per mile!\n");
+    let str;
+    if (direct) {
+        str = "a direct ";
+    } else {
+        str = "an indirect ";
+    }
+    let t = document.createTextNode(carrier + " has " + str + "flight to " + message + " on " + date + " for $" + minprice + "!\n");
     d.appendChild(t);
     document.body.appendChild(d);
 
